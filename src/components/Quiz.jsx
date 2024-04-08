82fdcd7625385e74aadd022da1886119d6cc2d2e
Jp Keys <jpmusik29@gmail.com>
2024-04-08 16:30:43 -0400

    Derived State setup

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([])
+
+    const activeQuestionIndex = userAnswers.length;
+    
     return (
         <p>Currently Active Question</p>
     )
