cc79e489f06e9b8c72959d6b24aa8c5178b2bb18
Jp Keys <jpmusik29@gmail.com>
2024-04-08 16:49:48 -0400

    Outputting questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length;
-    
+
+    function handleNextQuestion() {
+        setUserAnswers(current => current + 1);
+    }
+
     return (
-        <p>Currently Active Question</p>
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <button onClick={handleNextQuestion} disabled={userAnswers === activeQuestionIndex -1}>Next</button>
+        </div>
     )
 }
