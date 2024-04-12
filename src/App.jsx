88c8bfeb8f618836f4ac29216a6a382ff5a6d5e0
Jp Keys <jpmusik29@gmail.com>
2024-04-12 07:56:27 -0400

    Outputting questions

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Header from "./components/Header";
 import Quiz from "./components/Quiz";
 
 function App() {
-    return(
+    return (
         <div>
             <Header />
-            <Quiz />
+            <main>
+                <Quiz />
+            </main>
         </div>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,17 +3,30 @@ import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
-
     const activeQuestionIndex = userAnswers.length;
 
-    function handleNextQuestion() {
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    }
+
+    function handleNextQuestion(selectedAnswer) {
         setUserAnswers(current => current + 1);
     }
 
     return (
-        <div id="question">
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <button onClick={handleNextQuestion} disabled={userAnswers === activeQuestionIndex -1}>Next</button>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
