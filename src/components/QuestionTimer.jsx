c928b0cf6b198a1c0bfab1cceb8b43c92cbdeb1f
Jp Keys <jpmusik29@gmail.com>
2024-04-16 08:11:12 -0400

    Setting up timer

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function QuestionTimer ({timeout, onTimeout}) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    setTimeout (() => {
+        onTimeout();
+    }, timeout);
+
+    setInterval(() => {
+        setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+    }, 100);
+
+    return <progress />;
+}
